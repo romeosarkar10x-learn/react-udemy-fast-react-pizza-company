44a55bc96edb12bcb03a3fa4017b1d3f2d358672
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-26 21:49:59 +0530

    Initialized project. Replaced/removed example files.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,107 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <span>{props.price}</span>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Company</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12,
+    closeHour = 22;
+
+  const isOpen = openHour <= hour && hour <= closeHour;
+
+  function getTime() {
+    return new Date().toLocaleTimeString();
+  }
+
+  const [time, setTime] = React.useState(getTime());
+
+  React.useEffect(function () {
+    setInterval(function () {
+      setTime(getTime());
+    }, 20);
+  }, []);
+
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <>
+          <p>
+            [<time>{time}</time>] We're open until {closeHour}! Come visit us,
+            or order online.
+          </p>
+          <button className="btn">Order</button>
+        </>
+      )}
+    </footer>
+  );
+}
+
+function Menu() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(function () {
+    fetch("/data.json", {
+      method: "GET",
+    }).then(function (res) {
+      res.json().then((data) => {
+        console.log(data);
+        setPizzas(data.data);
+      });
+    });
+  }, []);
+
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <ul className="pizzas">
+        {pizzas.map(function (pizza) {
+          return (
+            <Pizza
+              name={pizza.name}
+              ingredients={pizza.ingredients}
+              price={pizza.price}
+              photoName={pizza.photoName}
+              soldOut={pizza.soldOut}
+            />
+          );
+        })}
+      </ul>
+    </main>
+  );
+}
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +111,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
